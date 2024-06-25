b987eb15627dbf3e0468e9917f6e4ece57edd2bf
iyuei <13142322+iyuei@user.noreply.gitee.com>
2024-06-25 17:31:37 +0800

    布局样式以及权重

--- Geeker-Admin/src/hooks/index.ts
+++ Geeker-Admin/src/hooks/index.ts
@@ -6,3 +6,5 @@ export { useAuthButtons } from "./modules/useAuthButtons"; // 按钮权限
 export { useOnline } from "./modules/useOnline"; // 网络是否可用
 export type { TableCustomColumnData } from "./modules/useTable";
 export { curryingRequest } from "./modules/useRequest"; // 请求封装
+export { useTime } from "./modules/useTime"; // 主题切换
+
--- Geeker-Admin/src/views/proTable/useTableDemo/table.ts
+++ Geeker-Admin/src/views/proTable/useTableDemo/table.ts
@@ -1,122 +1,123 @@
-import { ElTag, ElImage } from "element-plus";
-import { h } from "vue";
-import type { TableCustomColumnData } from "@/hooks";
-import commonFunction from "@/utils/commonFunction";
-import findOption from "@/utils/dict"; // 根据value查label
-import { ALL_TYPE_OPTIONS } from "./options";
+import { ElTag, ElImage } from 'element-plus';
+import { h } from 'vue';
+import type { TableCustomColumnData } from '@/hooks';
+import commonFunction from '@/utils/commonFunction';
+import findOption from '@/utils/dict'; // 根据value查label
+import { ALL_TYPE_OPTIONS } from './options';
 
 // 定义表头数据
 export const createTableColumns = (): TableCustomColumnData[] => {
-    // 解构出金额格式、文本格式、小数格式、时间格式方法
-    const { moneyFormat, textFormat, scaleFormat, dayjsFormat } = commonFunction();
-    return [
-        {
-            title: "序号",
-            key: "sortTableNo",
-            width: 80,
-            show: true,
-            align: "center",
-            headerAlign: "center",
-        },
-        {
-            title: "名称",
-            key: "name",
-            show: true,
-            tooltip: true,
-            align: "center",
-            headerAlign: "center",
-            formatter: (record) => {
-                return textFormat(record.name ? record.name : "暂无姓名");
-            },
-        },
-        {
-            title: "ID",
-            key: "id",
-            show: true,
-            align: "center",
-            headerAlign: "center",
-            formatter: (record) => {
-                return h(ElTag, { type: "success" }, { default: () => textFormat(record.id) });
-            },
-            renderHeader: (record) => {
-                return h(ElTag, { type: "success" }, { default: () => "ID" });
-            },
-        },
-        {
-            title: "状态",
-            width: 100,
-            slotName: "status",
-            key: "status",
-            show: true,
-            align: "center",
-        },
-        {
-            title: "金额",
-            key: "money",
-            show: true,
-            align: "right",
-            headerAlign: "center",
-            formatter: (record) => {
-                return h("span", { class: "text-red text-price" }, moneyFormat(scaleFormat(record.money, 2)));
-            },
-        },
-        {
-            title: "类型",
-            key: "type",
-            show: true,
-            align: "center",
-            headerAlign: "center",
-            formatter: (record) => {
-                return textFormat(findOption(ALL_TYPE_OPTIONS, record.type)?.label);
-            },
-        },
-        {
-            title: "地点",
-            key: "address",
-            show: true,
-            tooltip: true,
-            align: "center",
-            formatter: (record) => {
-                return textFormat(record.address);
-            },
-        },
-        {
-            title: "日期",
-            key: "date",
-            show: true,
-            align: "center",
-            headerAlign: "center",
-            formatter: (record) => {
-                return textFormat(dayjsFormat(record.date, "YYYY-MM-DD"));
-            },
-        },
-        {
-            title: "图片",
-            width: 120,
-            key: "image",
-            show: true,
-            align: "center",
-            formatter: (record) => {
-                if (record.image) {
-                    // 如果有图片，则使用 ElImage 组件并设置 preview-src-list
-                    return h(ElImage, {
-                        src: record.image,
-                        previewSrcList: [record.image], // 设置预览图片列表
-                        style: { width: "100px", height: "100px" },
-                    });
-                } else {
-                    // 如果没有图片，则返回一个占位符
-                    return h("span", "-");
-                }
-            },
-        },
-        {
-            title: "操作",
-            fixed: "right",
-            width: 220,
-            key: "operate",
-            show: true,
-            align: "center",
-        },
-    ];
+	// 解构出金额格式、文本格式、小数格式、时间格式方法
+	const { moneyFormat, textFormat, scaleFormat, dayjsFormat } = commonFunction();
+	return [
+		{
+			title: '序号',
+			key: 'sortTableNo',
+			width: 80,
+			show: true,
+			align: 'center',
+			headerAlign: 'center',
+		},
+		{
+			title: '名称',
+			key: 'name',
+			show: true,
+			tooltip: true,
+			align: 'center',
+			headerAlign: 'center',
+			formatter: (record) => {
+				return textFormat(record.name ? record.name : '暂无姓名');
+			},
+		},
+		{
+			title: 'ID',
+			key: 'id',
+			show: true,
+			align: 'center',
+			headerAlign: 'center',
+			formatter: (record) => {
+				return h(ElTag, { type: 'success' }, { default: () => textFormat(record.id) });
+			},
+		},
+		{
+			title: '状态',
+			width: 100,
+			slotName: 'status',
+			key: 'status',
+			show: true,
+			align: 'center',
+		},
+		{
+			title: '金额',
+			key: 'money',
+			show: true,
+			align: 'right',
+			headerAlign: 'center',
+			formatter: (record) => {
+				return h(
+					'span',
+					{ class: 'text-red text-price' },
+					moneyFormat(scaleFormat(record.money, 2))
+				);
+			},
+		},
+		{
+			title: '类型',
+			key: 'type',
+			show: true,
+			align: 'center',
+			headerAlign: 'center',
+			formatter: (record) => {
+				return textFormat(findOption(ALL_TYPE_OPTIONS, record.type)?.label);
+			},
+		},
+		{
+			title: '地点',
+			key: 'address',
+			show: true,
+			tooltip: true,
+			align: 'center',
+			formatter: (record) => {
+				return textFormat(record.address);
+			},
+		},
+		{
+			title: '日期',
+			key: 'date',
+			show: true,
+			align: 'center',
+			headerAlign: 'center',
+			formatter: (record) => {
+				return textFormat(dayjsFormat(record.date, 'YYYY-MM-DD'));
+			},
+		},
+		{
+			title: '图片',
+			width: 120,
+			key: 'image',
+			show: true,
+			align: 'center',
+			formatter: (record) => {
+				if (record.image) {
+					// 如果有图片，则使用 ElImage 组件并设置 preview-src-list
+					return h(ElImage, {
+						src: record.image,
+						previewSrcList: [record.image], // 设置预览图片列表
+						style: { width: '100px', height: '100px' },
+					});
+				} else {
+					// 如果没有图片，则返回一个占位符
+					return h('span', '-');
+				}
+			},
+		},
+		{
+			title: '操作',
+			fixed: 'right',
+			width: 220,
+			key: 'operate',
+			show: true,
+			align: 'center',
+		},
+	];
 };
