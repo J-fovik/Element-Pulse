90f9e71c8a290b22299e270ca56d9a7b1b78cd6d
yue <jcy1004888@163.com>
2025-08-08 12:06:37 +0800

    1

---
+++ Crush-Admin/src/components/Cropping/index.vue
@@ -0,0 +1,209 @@
+<template>
+	<div
+		v-if="inSrc"
+		ref="tippyElRef"
+		:class="{ 're-circled': inCircled }"
+		:style="getWrapperStyle"
+	>
+		<img
+			v-show="isReady"
+			ref="imgElRef"
+			:style="getImageStyle"
+			:src="inSrc"
+			:alt="props.alt"
+			:crossorigin="props.crossorigin"
+		/>
+	</div>
+</template>
+
+<script setup lang="ts" name="Cropping">
+import Cropper from 'cropperjs';
+import 'cropperjs/dist/cropper.css';
+import { useResizeObserver } from '@vueuse/core';
+import { debounce } from '@/utils/other';
+import { isArray } from '@/utils/type';
+import type { CSSProperties } from 'vue';
+
+type Options = Cropper.Options;
+// 默认配置
+const defaultOptions: Options = {
+	aspectRatio: 1,
+	zoomable: true,
+	zoomOnTouch: true,
+	zoomOnWheel: true,
+	cropBoxMovable: true,
+	cropBoxResizable: true,
+	toggleDragModeOnDblclick: true,
+	autoCrop: true,
+	background: true,
+	highlight: true,
+	center: true,
+	responsive: true,
+	restore: true,
+	checkCrossOrigin: true,
+	checkOrientation: true,
+	scalable: true,
+	modal: true,
+	guides: true,
+	movable: true,
+	rotatable: true,
+};
+// 父组件参数
+const props = defineProps({
+	src: { type: String, required: true }, // 图片路径
+	alt: { type: String }, // 无法显示时的替代文本
+	circled: { type: Boolean, default: false }, // 形状
+	realTimePreview: { type: Boolean, default: true }, // 是否支持实时预览
+	height: { type: [String, Number], default: '360px' }, // 图片样式高
+	crossorigin: {
+		type: String as PropType<'' | 'anonymous' | 'use-credentials' | undefined>,
+		default: undefined, // 跨域来源
+	},
+	imageStyle: { type: Object as PropType<CSSProperties>, default: () => ({}) }, // 图片样式
+	options: { type: Object as PropType<Options>, default: () => ({}) }, // 配置
+});
+// 父组件方法
+const emit = defineEmits(['readied', 'cropper', 'error']);
+const tippyElRef = ref<HTMLImageElement>(); // 包裹图片实例
+const imgElRef = ref<HTMLImageElement>(); // 图片实例
+const cropper = ref() as any; // 插件实例
+const inCircled = ref(props.circled); // 裁剪形状
+const inSrc = ref(props.src); // 图片路径
+const isReady = ref(false); // 是否可读
+const imgBase64 = ref(); // Base64路径（返回路径）
+let scaleX = 1;
+let scaleY = 1;
+// 防抖处理
+const debounceRealTimeCroppered = debounce(realTimeCroppered, 80);
+// 获取图片样式
+const getImageStyle = computed(
+	(): CSSProperties => ({
+		height: props.height,
+		maxWidth: '100%',
+		...props.imageStyle,
+	})
+);
+// 获取封面样式
+const getWrapperStyle = computed(
+	(): CSSProperties => ({
+		height: `${props.height}`.replace(/px/, '') + 'px',
+	})
+);
+// 实时预览
+function realTimeCroppered() {
+	props.realTimePreview && croppered();
+}
+// 处理裁剪器操作
+const handCropper = (event: string, arg?: number | Array<number>) => {
+	if (event === 'scaleX') {
+		scaleX = arg = scaleX === -1 ? 1 : -1;
+	}
+	if (event === 'scaleY') {
+		scaleY = arg = scaleY === -1 ? 1 : -1;
+	}
+	arg && isArray(arg) ? cropper.value?.[event]?.(...arg) : cropper.value?.[event]?.(arg);
+};
+/**
+ * 生成圆形裁剪画布（带抗锯齿和透明背景）
+ * @returns 圆形裁剪后的Canvas对象
+ * @throws 当裁剪器未初始化或获取画布失败时抛出错误
+ */
+const getRoundedCanvas = () => {
+	const sourceCanvas = cropper.value!.getCroppedCanvas();
+	const canvas = document.createElement('canvas');
+	const context = canvas.getContext('2d')!;
+	const width = sourceCanvas.width;
+	const height = sourceCanvas.height;
+	canvas.width = width;
+	canvas.height = height;
+	context.imageSmoothingEnabled = true;
+	context.drawImage(sourceCanvas, 0, 0, width, height);
+	context.globalCompositeOperation = 'destination-in';
+	context.beginPath();
+	context.arc(width / 2, height / 2, Math.min(width, height) / 2, 0, 2 * Math.PI, true);
+	context.fill();
+	return canvas;
+};
+// 处理图片裁剪并返回结果的函数
+const croppered = () => {
+	if (!cropper.value) return;
+	const canvas = inCircled.value ? getRoundedCanvas() : cropper.value.getCroppedCanvas();
+	canvas.toBlob((blob) => {
+		if (!blob) return;
+		const fileReader: FileReader = new FileReader();
+		fileReader.readAsDataURL(blob);
+		fileReader.onloadend = (e) => {
+			if (!e.target?.result || !blob) return;
+			imgBase64.value = e.target.result;
+			emit('cropper', {
+				base64: e.target.result,
+				blob,
+				info: { size: blob.size, ...cropper.value.getData() },
+			});
+		};
+		fileReader.onerror = () => {
+			emit('error');
+		};
+	});
+};
+// 上传前校验
+const beforeUpload = (file) => {
+	const reader = new FileReader();
+	reader.readAsDataURL(file);
+	inSrc.value = '';
+	reader.onload = (e) => {
+		inSrc.value = e.target?.result as string;
+	};
+	reader.onloadend = () => {
+		init();
+	};
+	return false;
+};
+// 初始化
+const init = async () => {
+	const imgEl = unref(imgElRef);
+	if (!imgEl) return;
+	cropper.value = new Cropper(imgEl, {
+		...defaultOptions,
+		ready: () => {
+			isReady.value = true;
+			realTimeCroppered();
+			emit('readied', cropper.value);
+		},
+		crop() {
+			debounceRealTimeCroppered();
+		},
+		zoom() {
+			debounceRealTimeCroppered();
+		},
+		cropmove() {
+			debounceRealTimeCroppered();
+		},
+		...props.options,
+	});
+};
+// 初始化zhixing
+onMounted(init);
+// 卸载
+onUnmounted(() => {
+	cropper.value?.destroy();
+	isReady.value = false;
+	cropper.value = null;
+	imgBase64.value = '';
+	scaleX = 1;
+	scaleY = 1;
+});
+// 监听元素尺寸变化并在变化时重置裁剪器
+useResizeObserver(tippyElRef, () => handCropper('reset'));
+</script>
+
+<style scoped lang="scss">
+.re-circled {
+	// 如果你的 .cropper-view-box 和 .cropper-face 是子组件内部的元素，
+	// 可能需要使用 :deep() 穿透作用域样式
+	:deep(.cropper-view-box),
+	:deep(.cropper-face) {
+		border-radius: 50%;
+	}
+}
+</style>
--- Crush-Admin/src/views/assembly/cropImage/index.vue
+++ Crush-Admin/src/views/assembly/cropImage/index.vue
@@ -32,6 +32,58 @@
 			</el-descriptions>
 		</el-card>
 
+		<el-card shadow="hover" header="Cropping 裁剪图片" class="mb20">
+			<el-alert
+				title="感谢优秀的 `cropperjs`，本 Demo 是基于：https://mraining.github.io/Cropperjs.html"
+				type="warning"
+				:closable="false"
+				class="mb15"
+			/>
+			<div style="display: flex; align-items: center; justify-content: center">
+				<el-popover placement="right" width="300px">
+					<template #reference>
+						<Cropping
+							:src="state.vueCropperImg"
+							circled
+							style="width: 30vw"
+							@cropper="updateCropper"
+							@readied="() => {}"
+						/>
+					</template>
+					<div class="flex flex-wrap justify-center items-center text-center">
+						<el-image
+							v-if="state.cropperImg"
+							:src="state.cropperImg"
+							:preview-src-list="Array.of(state.cropperImg)"
+							fit="cover"
+						/>
+						<div v-if="state.infos" class="mt-1">
+							<p>
+								图像大小：{{ parseInt(state.infos.width) }} ×
+								{{ parseInt(state.infos.height) }}像素
+							</p>
+							<p>
+								文件大小：{{ sizeToStr(state.infos.size) }}（{{ state.infos.size }}
+								字节）
+							</p>
+						</div>
+					</div>
+				</el-popover>
+			</div>
+			<el-descriptions :column="1" border class="mt20">
+				<el-descriptions-item label="src">图片路径</el-descriptions-item>
+				<el-descriptions-item label="alt">无法显示时的替代文本</el-descriptions-item>
+				<el-descriptions-item label="circled">形状</el-descriptions-item>
+				<el-descriptions-item label="realTimePreview">
+					是否支持实时预览
+				</el-descriptions-item>
+				<el-descriptions-item label="height">图片样式高</el-descriptions-item>
+				<el-descriptions-item label="crossorigin">跨域来源</el-descriptions-item>
+				<el-descriptions-item label="imageStyle">图片样式</el-descriptions-item>
+				<el-descriptions-item label="options">配置</el-descriptions-item>
+			</el-descriptions>
+		</el-card>
+
 		<el-card shadow="hover" header="VueCropper 裁剪图片" class="mb20">
 			<el-alert
 				title="感谢优秀的 `vue-cropper`，本 Demo 是基于：https://vue-cropper.vercel.app/#/demo-crop"
@@ -70,12 +122,15 @@
 
 <script setup lang="ts" name="cropImage">
 import { useBasicsState } from '@/hooks';
-import { downloadBase64Image } from '@/utils/fileOperation';
+import { downloadBase64Image, sizeToStr } from '@/utils/fileOperation';
+
 // 页面唯一元素控制
 const [activeKey, setActiveKey] = useBasicsState<string | null>(null);
 const state = reactive({
 	cropperJsImg:
 		'https://img2.baidu.com/it/u=1978192862,2048448374&fm=253&fmt=auto&app=138&f=JPEG?w=504&h=500',
+	cropperImg: '/src/assets/images/avatar.png', // 裁剪图片的地址
+	infos: {} as any, // 信息
 	vueCropperImg: '/src/assets/images/avatar.png', // 裁剪图片的地址
 });
 
@@ -83,6 +138,13 @@ const state = reactive({
 const updateCropperJsImg = (img: any) => {
 	state.cropperJsImg = img;
 	// downloadBase64Image(img, 'cropperJsImg.png');
+};
+
+// 更新Cropper图片
+const updateCropper = ({ base64, blob, info }) => {
+	console.log(blob);
+	state.infos = info;
+	state.cropperImg = base64;
 };
 
 // 更新VueCropper图片
